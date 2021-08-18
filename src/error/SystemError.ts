f57f849be9169d7f00849721a302db360b8b17a8
HillTopTRPG <mihikari90@gmail.com>
2021-08-18 18:22:47 +0900

    :sparkles: とりあえず起動はできるようになった

--- src/error/SystemError.ts
+++
@@ -1,9 +0,0 @@
-export class SystemError implements Error {
-  public name = "SystemError";
-
-  constructor(public message: string) {}
-
-  public toString() {
-    return `${this.name}: ${this.message}`;
-  }
-}
